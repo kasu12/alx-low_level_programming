c7f6431aa64e79e4a827ace4b8d6e6138c4f3373
kasu12 <kasu121921@gmail.com>
2022-12-09 04:35:20 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int num = 0;
+
+	while (num != 10)
+	{
+		printf(num);
+		num++
+	}
+	
+	return (0);
+}
