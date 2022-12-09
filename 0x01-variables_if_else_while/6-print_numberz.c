7b2613cc52ef2c9cfbddd213aa99bc22c4d15012
kasu12 <kasu121921@gmail.com>
2022-12-09 05:32:48 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succcess)
+*/
+int main(void)
+{
+	int num = 48;
+
+	while (num <= 57)
+	{
+		putchar((char)num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
+
