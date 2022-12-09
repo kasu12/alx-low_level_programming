9da4a420e78d75f5fc024e96939ccbdb5b701c70
kasu12 <kasu121921@gmail.com>
2022-12-09 01:34:07 -0800

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,15 +16,15 @@ int main(void)
 			n = rand() - RAND_MAX / 2;
 				if (n > 0)
 				{
-					puts("is positive")
+					puts("is positive");
 				}
 				else if (n == 0)
 				{
-					puts("is zero")
+					puts("is zero");
 				}
 				else
 				{
-					puts("is negative")
+					puts("is negative");
 				}
 					return (0);
 }
