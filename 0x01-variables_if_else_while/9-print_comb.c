9b04bfaa6613d0ba7f2bd94eccfebe16d9893b43
kasu12 <kasu121921@gmail.com>
2022-12-09 05:55:20 -0800

    printing values with space and comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar((char)num);
+		if (num == 9)
+			break;
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+	return (0);
+}
